41737ead4ad25c3afef9d607d199d8ef117631a7
dukesvrtech <horace.reid@bluenetreflections.com>
2025-02-16 08:42:51 -0500

    move store to root level

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,6 @@
 import { auth } from "@/app/(auth)/auth"
 import { redirect } from "next/navigation"
 import { ADMIN_ROLES } from "@/app/constants/user-roles"
-import { Providers } from "./context/Providers"
 import { AdminSidebar } from "./components/AdminSidebar"
 
 export default async function AdminLayout({
@@ -19,15 +18,13 @@ export default async function AdminLayout({
   }
 
   return (
-    <Providers>
-      <div className="flex">
-        <AdminSidebar />
-        <main className="flex-1 overflow-y-auto pl-16">
-          <div className="mt-12">
-            {children}
-          </div>
-        </main>
-      </div>
-    </Providers>
+    <div className="flex">
+      <AdminSidebar />
+      <main className="flex-1 overflow-y-auto pl-16">
+        <div className="mt-12">
+          {children}
+        </div>
+      </main>
+    </div>
   )
 } 
